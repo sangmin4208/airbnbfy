5885ddb715076a92662187187580bde5a2d7601c
Lee Sang-Min <sangmin4208@gmail.com>
2021-11-24 15:29:28 +0900

    feat: Change the way to obfuscate
    Now, user input text is obfuscated without button click.

--- js/main.js
+++ js/main.js
@@ -1,43 +1,52 @@
 const $originText = document.getElementById("origin-text");
 const $obfuscatedText = document.getElementById("obfuscated-text");
-const $btn = document.getElementById("btn")
 
 function init() {
-    $btn.addEventListener("click", ()=>{
-        console.log($originText.value)
-        let result = obfuscatedText($originText.value)
-        $obfuscatedText.value = result
-    })
-    test()
+  obfuscates();
+
 }
 init();
 
-// obfuscatedText
-// input text
-// obfuscates the text and return
-function obfuscatedText(origin) {
-  let result = new Array(origin.length);
-  origin.split("").forEach((v, i) => {
-    result[i] = obfuscatedChar(v);
+function obfuscates() {
+  let qr_timeout = null;
+  $originText.addEventListener("input", () => {
+    if (qr_timeout != null) {
+      clearTimeout(qr_timeout);
+    }
+    qr_timeout = setTimeout(function () {
+      console.log($originText.value);
+      let result = obfuscatedText($originText.value);
+      $obfuscatedText.innerText = result;
+    }, 500);
   });
-  return result.join("")
+  // obfuscatedText
+  // input text
+  // obfuscates the text and return
+  function obfuscatedText(origin) {
+    let result = new Array(origin.length);
+    origin.split("").forEach((v, i) => {
+      result[i] = obfuscatedChar(v);
+    });
+    return result.join("");
+  }
+
+  // obfuscatedChar
+  // input only one character
+  // obfuscates charater and return
+  function obfuscatedChar(char) {
+    const firstCode = 44032;
+    const lastCode = firstCode + 11171;
+    let cc = char.charCodeAt(0);
+    // if it is not korean just return origin character
+    if (!(cc >= firstCode && cc <= lastCode)) {
+      return char;
+    }
+    return String.fromCharCode(cc + rand());
+  }
+  // rand return a number from 1 to 28
+  function rand() {
+    return Math.floor(Math.random() * 4) + 1;
+  }
 }
 
-// obfuscatedChar
-// input only one character
-// obfuscates charater and return
-function obfuscatedChar(char) {
-  const firstCode = 44032
-  const lastCode = firstCode+11171
-  let cc = char.charCodeAt(0);
-  // if it is not korean just return origin character
-  if (!(cc >=firstCode && cc <=lastCode)){
-    return char
-  }
-  return String.fromCharCode(cc + rand());
-}
-// rand return a number from 1 to 28
-function rand() {
-    
-  return  Math.floor(Math.random()*28) + 1
-}
+
